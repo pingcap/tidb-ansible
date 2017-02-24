d73d77fe05a1fba9526ce38c72fbe0729e679ba2
andelf <andelf@gmail.com>
2017-02-24 20:17:09 +0800

    pd/tikv/grafana: update for newest pd/tikv

--- filter_plugins/epoch_timestamp_diff.py
+++ filter_plugins/epoch_timestamp_diff.py
@@ -10,8 +10,9 @@ def epoch_time_diff(t):
 def with_default_dicts(d, *args):
     ret = copy.deepcopy(d) or {}
     for arg in args:
-        ret.update({k: with_default_dicts(ret[k], arg[k]) for k in arg if k in ret and isinstance(ret[k], (dict, type(None))) })
-        ret.update({k: arg[k] for k in arg if k not in ret})
+        if arg:
+            ret.update({k: with_default_dicts(ret[k], arg[k]) for k in arg if k in ret and isinstance(ret[k], (dict, type(None))) })
+            ret.update({k: arg[k] for k in arg if k not in ret})
     return ret
 
 class FilterModule(object):
