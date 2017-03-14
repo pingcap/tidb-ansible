bdaf2b66cf891a8646f302ccad74fc4b8a4a060c
andelf <andelf@gmail.com>
2017-03-14 14:15:57 +0800

    cloud: fix massive bugs

--- filter_plugins/tags.py
+++ filter_plugins/tags.py
@@ -2,7 +2,6 @@
 import time
 import copy
 
-from ansible import errors
 
 def epoch_time_diff(t):
     return int(int(t) - time.time())
@@ -11,8 +10,9 @@ def with_default_dicts(d, *args):
     ret = copy.deepcopy(d) or {}
     for arg in args:
         if arg:
-            ret.update({k: with_default_dicts(ret[k], arg[k]) for k in arg if k in ret and isinstance(ret[k], (dict, type(None))) })
-            ret.update({k: arg[k] for k in arg if k not in ret})
+            ret.update([(k, with_default_dicts(ret[k], arg[k]))
+                        for k in arg if k in ret and isinstance(ret[k], (dict, type(None)))])
+            ret.update([(k, arg[k]) for k in arg if k not in ret])
     return ret
 
 class FilterModule(object):
