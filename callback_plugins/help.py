9a81472e265e8e56ee78dcf5e3f944d0722f0135
Louis <gofaceme@gmail.com>
2018-01-10 16:08:32 +0800

    help plugin

--- callback_plugins/help.py
+++ callback_plugins/help.py
@@ -13,6 +13,7 @@ DOCUMENTATION = '''
 
 from ansible.plugins.callback import CallbackBase
 from ansible import constants as C
+import os
 
 class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
@@ -20,12 +21,19 @@ class CallbackModule(CallbackBase):
     CALLBACK_NAME = 'help'
     CALLBACK_NEEDS_WHITELIST = True
 
-    def v2_runner_on_failed(self, result, ignore_errors=False):
+    def __init__(self):
+
+        super(CallbackModule, self).__init__()
+
+        if not os.path.exists(os.path.dirname(C.DEFAULT_LOG_PATH)):
+            os.makedirs(os.path.dirname(C.DEFAULT_LOG_PATH))
+    def print_help_message(self):
         self._display.display("Ask for help:", color=C.COLOR_WARN)
         self._display.display("sre@pingcap.com", color=C.COLOR_HIGHLIGHT)
         self._display.display("Send an email to the above address, attached with the tidb-ansible/inventory.ini and tidb-ansible/log/ansible.log files and the error message.", color=C.COLOR_WARN)
 
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self.print_help_message()
+
     def v2_runner_on_unreachable(self, result):
-        self._display.display("Ask for help:", color=C.COLOR_WARN)
-        self._display.display("sre@pingcap.com", color=C.COLOR_HIGHLIGHT)
-        self._display.display("Send an email to the above address, attached with the tidb-ansible/inventory.ini and tidb-ansible/log/ansible.log files and the error message.", color=C.COLOR_WARN)
+        self.print_help_message()
