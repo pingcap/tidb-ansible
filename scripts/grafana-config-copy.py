1ec4c3f22183363a5daf54698ac03788f739521f
liubo0127 <liubo0127@qq.com>
2018-10-26 18:29:24 +0800

    add lightning monitor dashboard and alert

--- scripts/grafana-config-copy.py
+++ scripts/grafana-config-copy.py
@@ -28,7 +28,8 @@ src = dict(
                 "overview": 'overview.json',
                 "disk_performance": 'disk_performance.json',
                 "blackbox_exporter": "blackbox_exporter.json",
-                "kafka_overview": "kafka.json"})
+                "kafka_overview": "kafka.json",
+                "lightning": "lightning.json"})
 
 dests = [
 ]
