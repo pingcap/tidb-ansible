0e0aea3163353644ba187fdb5b20237f00d03281
liubo0127 <liubo0127@qq.com>
2019-05-16 11:38:42 +0800

    Adjust output format

--- output.tf
+++ output.tf
@@ -1,23 +1,23 @@
 output "tidb" {
-  value = "${join("\n", aws_instance.tidb.*.private_ip)}"
+  value = "${join(", ", aws_instance.tidb.*.private_ip)}"
 }
 
 output "tikv" {
-  value = "${join("\n", aws_instance.tikv.*.private_ip)}"
+  value = "${join(", ", aws_instance.tikv.*.private_ip)}"
 }
 
 output "pd" {
-  value = "${join("\n", aws_instance.pd.*.private_ip)}"
+  value = "${join(", ", aws_instance.pd.*.private_ip)}"
 }
 
 output "bastion_ip" {
-  value = "${join("\n", aws_eip.bastion.*.public_ip)}"
+  value = "${join(", ", aws_eip.bastion.*.public_ip)}"
 }
 
 output "monitor-dns" {
-  value = "${join("\n", module.aws-elb.monitor-dns)}"
+  value = "${join(", ", module.aws-elb.monitor-dns)}"
 }
 
 output "tidb-dns" {
-  value = "${join("\n", module.aws-elb.tidb-dns)}"
+  value = "${join(", ", module.aws-elb.tidb-dns)}"
 }
