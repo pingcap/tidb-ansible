caa2830cc5bea23e420af6ffee846f9d43b6071e
Jack Yu <jackysp@gmail.com>
2019-07-25 15:52:30 +0800

    script: add a new tidb summary dashboard (#852)

--- scripts/grafana-config-copy.py
+++ scripts/grafana-config-copy.py
@@ -30,6 +30,7 @@ src = dict(
     dashboards={"node": 'node.json',
                 "pd"  : 'pd.json',
                 "tidb": 'tidb.json',
+                "tidb_summary": 'tidb_summary.json',
                 "tikv_summary": 'tikv_summary.json',
                 "tikv_details": 'tikv_details.json',
                 "tikv_trouble_shot": 'tikv_trouble_shooting.json',
