49d848c49d6cb847de82a13db43d76f699f60a27
liubo <liubo0127@qq.com>
2020-01-09 17:06:50 +0800

    update

--- filter_plugins/tags.py
+++ filter_plugins/tags.py
@@ -3,6 +3,8 @@
 import time
 import copy
 import json
+import operator as py_operator
+from distutils.version import LooseVersion, StrictVersion
 
 def epoch_time_diff(t):
     return int(int(t) - time.time())
@@ -22,22 +24,32 @@ def update_default_dicts(d):
         ret.update([(k, update_default_dicts(ret[k])) for k in ret if isinstance(ret[k], (dict, type(None)))])
     return ret
 
-def ansible_version_compare(d, *args):
-    if args[1] == '<=':
-        if str(d) <= args[0]:
-            return True
-    elif args[1] == '<':
-        if str(d) < args[0]:
-            return True
-    elif args[1] == '>=':
-        if str(d) >= args[0]:
-            return True
-    elif args[1] == '>':
-        if str(d) > args[0]:
-            return True
-    elif args[1] == '==':
-        if str(d) == args[0]:
-            return True
+def ansible_version_compare(d, version, operator, strict=False):
+    op_map = {
+        '==': 'eq', '=': 'eq', 'eq': 'eq',
+        '<': 'lt', 'lt': 'lt',
+        '<=': 'le', 'le': 'le',
+        '>': 'gt', 'gt': 'gt',
+        '>=': 'ge', 'ge': 'ge',
+        '!=': 'ne', '<>': 'ne', 'ne': 'ne'
+    }
+    if strict:
+        Version = StrictVersion
+    else:
+        Version = LooseVersion
+
+    if operator in op_map:
+        operator = op_map[operator]
+    else:
+        print 'Invalid operator type'
+        raise
+
+    try:
+        method = getattr(py_operator, operator)
+        return method(Version(str(d)), Version(str(version)))
+    except Exception as e:
+        print 'Version comparison: {}'.format(e)
+        raise
 
 def dictsort_by_value_type(d):
     vals = list(d.items())
