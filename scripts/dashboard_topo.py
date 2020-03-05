c0e2594c0eff96a1961623754b38cf8d104c5a2c
mapleFU <1506118561@qq.com>
2020-03-05 10:14:30 +0800

    [Support] change tidb-ansible script in tidb-ansible.
    Signed-off-by: mapleFU <1506118561@qq.com>

--- scripts/dashboard_topo.py
+++ scripts/dashboard_topo.py
@@ -9,7 +9,7 @@ import base64
 import json
 import argparse
 
-ComponentToRegister = ('alertmanager', 'grafana', 'pd')
+ComponentToRegister = ('alertmanager', 'grafana', 'pd', 'prometheus')
 
 
 def parse_opts():
@@ -93,20 +93,17 @@ if __name__ == '__main__':
     args = parse_opts()
 
     # parse from args
-    # pd_address = concat_to_address(args.pd_host, args.pd_port)
-    # tidb_address = concat_to_address(args.tidb_host, args.tidb_port)
-    # alertmanager_address = concat_to_address(args.alertmanager_host,
-    #                                          args.alertmanager_port)
-    # grafana_address = concat_to_address(args.grafana_host, args.grafana_port)
     pd_address = args.pd
     pd_address_zero, _ = parse_address(pd_address.split(',')[0])
 
     alertmanager_address = args.alertmanager
     grafana_address = args.grafana
+    prometheus_address = args.prometheus
 
     mapping = {
         'alertmanager': alertmanager_address,
         'grafana': grafana_address,
+        'prometheus': prometheus_address,
     }
 
     for comp in ComponentToRegister:
